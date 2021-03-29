ee9f47dbda7d9f278a1dec364c4b349f6d9f4fab
Lori Lee <zachtownleysmith@yahoo.com>
2021-03-28 23:02:37 -0500

    added a route

--- flask_end.py
+++ flask_end.py
@@ -6,11 +6,17 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/hello', methods = ['GET', 'POST'])
+@app.route('/hello', methods=['GET', 'POST'])
 def hello_word():
     return "Hello World"
 
 
-#Testing Block
+@app.route('/hello/<name>', methods=['GET', 'POST'])
+def hello_word2(name):
+    return "Hello, {}".format(name)
+
+
+# Testing Block
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
+
